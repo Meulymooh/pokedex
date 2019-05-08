4d351cbd24e789a5fbfe5fb6ed75646df5c742a6
Emilie Mesureur <mesureur.e@gmail.com>
2019-05-08 14:35:35 +0200

    update

--- javascript.js
+++ javascript.js
@@ -1,38 +1,45 @@
 function getPokemon() {
 
-  // HTML div variables
+  // API variable
+  var api_1 = "https://pokeapi.co/api/v2/pokemon/";
+  var api_2 = "https://pokeapi.co/api/v2/pokemon-species/";
+
+  // HTML variable
   var input = document.getElementById("input").value;
-  var url = "https://pokeapi.co/api/v2/pokemon/";
-  // API variables
   var id = document.getElementById("id");
   var identity = document.getElementById("identity");
   var picture = document.getElementById("pic");
   var moves = document.getElementById("moves");
-  // var evolution = document.getElementById("evolution");
   var screenText1 = document.getElementById("screenText1");
+  var screenText2 = document.getElementById("screenText2");
+  var screenText3 = document.getElementById("screenText3");
+  var picture2 = document.getElementById("pic2");
   
   if (input != null) {
-    // Empty fields
-	id.innerHTML = "";
-	identity.innerHTML = "";
-	picture.innerHTML = "";
-	moves.innerHTML = "";
+  
+    // Empty all fields before searching
+    id.innerHTML = "";
+    identity.innerHTML = "";
+    picture.innerHTML = "";
+    picture2.innerHTML = "";    
+    moves.innerHTML = "";
+    screenText1.style.display = "";
+    screenText2.style.display = "";
+    screenText3.style.display = "";
 
     // Ajax request
     var xhr = new XMLHttpRequest();
     
-    xhr.open("GET", url + input, true);
+    xhr.open("GET", api_1 + input, true);
     xhr.onload = function() {
       var data = JSON.parse(xhr.response);
       
-
-
       // Make text div appear
       nameText.style.display = "inline-block";
       moveText.style.display = "inline-block";
       idText.style.display = "inline-block";
-      // evolutionText.style.display = "inline-block";
       screenText1.style.display = "block";
+      screenText2.style.display = "block";
 
       // Make Pokemon details appear
       id.innerHTML = data.id;
@@ -41,7 +48,31 @@ function getPokemon() {
       for (let i = 0; i < 4; i++) {
       moves.innerHTML += data.moves[i].move.name;
       };
-      // evolution.innerHTML = "";
+      
+      // Get previous evolution name
+      var evolution = data.species.name;
+        
+        if (evolution) {
+          var xhr2 = new XMLHttpRequest();
+              xhr2.open('GET', api_2 + evolution, true);
+              xhr2.onload = function() {
+                var data2 = JSON.parse(xhr2.response);
+                screenText3.innerHTML = data2.evolves_from_species.name;
+
+                // Get previous evolution picture
+                var evolutionName = data2.evolves_from_species.name;
+                  if (evolutionName) {
+                    var xhr3 = new XMLHttpRequest();
+                        xhr3.open('GET', api_1 + evolutionName, true);
+                        xhr3.onload = function() {
+                        var data = JSON.parse(xhr.response);
+                        pic2.innerHTML = "<img class='image' src='" + data.sprites.front_shiny + "'/>";
+                        } 
+                    xhr3.send();
+                  };
+              } 
+              xhr2.send();
+        };
     }    
   } else {
     alert("This Pokemon doesn't exist");
@@ -50,3 +81,5 @@ function getPokemon() {
 }
 
 document.getElementById("search").addEventListener("click", getPokemon);
+
+
--- readme.md
+++ readme.md
@@ -2,6 +2,15 @@ Link: https://meulymooh.github.io/pokedex/index.html
 
 # Pokedex
 
+## Coding
+
+* Only lower case letters accepted. Haven't solved how to take into account capital letters.
+* Three XMLhttpRequest: one for the pokemon, one for the name of the previous evolution, one for the picture of the previous evolution.
+
+## Styling
+
+* Buttons are just for decoration.
+
 ## Resources
 
 * Json: https://www.w3schools.com/js/js_json_intro.asp
@@ -9,3 +18,4 @@ Link: https://meulymooh.github.io/pokedex/index.html
 * How to make a Pokedex? https://fr.slideshare.net/Autodesk/make-your-own-pokedex-with-the-pokeapi-nodeexpress
 * Postman software
 
+	                                                                                                                                                                                                                                                                                                                                                                                                                               
